d31381ad0572cb7f7fe93377bb0e160823e50e69
Per Storgaard Laursen <per.s.laursen@live.dk>
2024-08-25 14:35:21 +0200

    Updates to Chap/Prog02
    Very small updates to Presentations
    Notes extended with section on HashSet
    All projects updated to .Net 8, EXCEPT projects including Unit Tests!

---
+++ Chap/Prog02/Solved/HashSetBaseCamp/Program.cs
@@ -0,0 +1,70 @@
+﻿
+HashSet<int> setA = new HashSet<int>();
+setA.Add(12);
+setA.Add(43);
+setA.Add(17);
+setA.Add(98);
+setA.Add(66);
+
+HashSet<int> setB = new HashSet<int>();
+setB.Add(66);
+setB.Add(20);
+setB.Add(43);
+setB.Add(19);
+setB.Add(81);
+
+HashSet<int> setC = new HashSet<int>();
+setC.Add(66);
+setC.Add(19);
+setC.Add(81);
+
+
+// Sets
+PrintCollection("Set A", setA);
+PrintCollection("Set B", setB);
+PrintCollection("Set C", setC);
+
+// Union
+IEnumerable<int> union = setA.Union(setB);
+PrintCollection("Union", union);
+
+// Intersection
+IEnumerable<int> intersection = setA.Intersect(setB);
+PrintCollection("Intersection", intersection);
+
+// Complement
+IEnumerable<int> complement = setA.Except(setB);
+PrintCollection("Complement", complement);
+
+// SuperSet
+bool superSetAC = setA.IsSupersetOf(setC);
+Console.WriteLine();
+Console.WriteLine($"A is SuperSet of C {superSetAC}");
+
+bool superSetBC = setB.IsSupersetOf(setC);
+Console.WriteLine();
+Console.WriteLine($"B is SuperSet of C {superSetBC}");
+
+// SubSet
+bool subSetCA = setC.IsSubsetOf(setA);
+Console.WriteLine();
+Console.WriteLine($"C is SubSet of A {subSetCA}");
+
+bool subSetCB = setC.IsSubsetOf(setB);
+Console.WriteLine();
+Console.WriteLine($"C is SubSet of B {subSetCB}");
+
+
+void PrintCollection(string text, IEnumerable<int> collection)
+{
+    Console.WriteLine();
+    Console.Write($"{text} (Count = {collection.Count()}) :   [ ");
+
+    foreach (int val in collection)
+    {
+        Console.Write($" {val} ");
+    }
+
+    Console.Write($"]");
+    Console.WriteLine();
+}
---
+++ Chap/Prog02/Unsolved/HashSetBaseCamp/Program.cs
@@ -0,0 +1,50 @@
+﻿
+HashSet<int> setA = new HashSet<int>();
+setA.Add(12);
+setA.Add(43);
+setA.Add(17);
+setA.Add(98);
+setA.Add(66);
+
+HashSet<int> setB = new HashSet<int>();
+setB.Add(66);
+setB.Add(20);
+setB.Add(43);
+setB.Add(19);
+setB.Add(81);
+
+HashSet<int> setC = new HashSet<int>();
+setC.Add(66);
+setC.Add(19);
+setC.Add(81);
+
+
+// Sets
+PrintCollection("Set A", setA);
+PrintCollection("Set B", setB);
+PrintCollection("Set C", setC);
+
+// Union - TODO
+
+// Intersection - TODO
+
+// Complement - TODO
+
+// SuperSet - TODO
+
+// SubSet - TODO
+
+
+void PrintCollection(string text, IEnumerable<int> collection)
+{
+    Console.WriteLine();
+    Console.Write($"{text} (Count = {collection.Count()}) :   [ ");
+
+    foreach (int val in collection)
+    {
+        Console.Write($" {val} ");
+    }
+
+    Console.Write($"]");
+    Console.WriteLine();
+}
